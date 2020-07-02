22a2a3054c29e0113d76829bd9e36d9bd313040b
jprangel <jpmrangel22@gmail.com>
2020-07-02 11:48:23 -0300

    Vue Tutorial

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" name="title" v-model="title" placeholder="Add Todo">
+      <input type="submit" value="submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      title: '',
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        title: this.title,
+        completed: false,
+      };
+      // Emitindo o novo TODO para os componentes "pai"
+      this.$emit('add-todo', newTodo);
+      this.title = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="text"] {
+    flex: 2;
+  }
+</style>
